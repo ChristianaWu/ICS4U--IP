5e748954759fffeb689520c34e05e605c53740d0
Christiana Zheng Wu <cz2wu@edu.uwaterloo.ca>
2019-09-11 14:09:11 -0400

    commit

--- msci240/src/msci240/Fraction.java
+++ msci240/src/msci240/Fraction.java
@@ -1,6 +1,34 @@
 package msci240;
 
 public class Fraction {
-    int numerator;
-    int denominator;
+	 private int numerator;
+	    private int denominator;
+	   
+	    public Fraction () {
+	    	this.numerator = 0;
+	    	this.denominator = 1;
+	    }
+	    
+	    public Fraction (int numerator, int denominator) {
+	    	this.denominator = denominator;
+	    	this.numerator = numerator;
+	    }
+	    
+	    public double asDouble() {
+	    	return (double) this.numerator/this.denominator;
+	    }
+	    
+	    public int getNumerator () {
+	    	return this.numerator;
+	    }
+	    
+	    public int getDenominator () {
+	    	return this.denominator;
+	    }
+	    
+	    public void simplify() {
+	    	int gcd = MathUtilities.gcd(this.numerator, this.denominator);
+	    	this.numerator = this.numerator % gcd;
+	    	this.denominator = this.denominator % gcd;
+	    }
 }
