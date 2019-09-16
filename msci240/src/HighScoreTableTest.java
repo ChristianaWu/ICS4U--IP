9f8b366b448f5d68c01d6a41cd5673d8c2afcedd
Christiana Zheng Wu <cz2wu@edu.uwaterloo.ca>
2019-09-15 22:08:47 -0400

    commit

---
+++ msci240/src/HighScoreTableTest.java
@@ -0,0 +1,46 @@
+
+
+import junit.framework.TestCase;
+
+public class HighScoreTableTest extends TestCase {
+
+    public void testHighScoreTable() {
+        fail("Not yet implemented");
+    }
+
+    public void testHighScoreTableInt() {
+        fail("Not yet implemented");
+    }
+
+    public void testSize() {
+        fail("Not yet implemented");
+    }
+
+    public void testGetCapacity() {
+        fail("Not yet implemented");
+    }
+
+    public void testAdd() {
+        fail("Not yet implemented");
+    }
+
+    public void testGetName() {
+        fail("Not yet implemented");
+    }
+
+    public void testGetScore() {
+        fail("Not yet implemented");
+    }
+
+    public void testWrite() {
+        fail("Not yet implemented");
+    }
+
+    public void testPrint() {
+        fail("Not yet implemented");
+    }
+
+    public void testRead() {
+        fail("Not yet implemented");
+    }
+}
