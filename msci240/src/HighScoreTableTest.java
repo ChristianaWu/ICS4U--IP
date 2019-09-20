85ecf5b59dc49cb6b0661abb01e4e9dd43283a75
Christiana Zheng Wu <cz2wu@edu.uwaterloo.ca>
2019-09-19 20:11:18 -0400

    commit

--- msci240/src/HighScoreTableTest.java
+++ msci240/src/HighScoreTableTest.java
@@ -1,4 +1,9 @@
-
+/*
+ * Christiana Wu
+ * 20767703
+ * Testing for the HighScoreTable class
+ * September 18, 2019
+ */
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -7,25 +12,28 @@ import junit.framework.TestCase;
 
 public class HighScoreTableTest extends TestCase {
 	
-	
+	//No parameter passed
     public void testHighScoreTable() {
        HighScoreTable x = new HighScoreTable (); 
        
        assertEquals (10,x.getCapacity());
     }
-
+    
+    //Proper Parameter passed
     public void testHighScoreTableInt() {
     	HighScoreTable x = new HighScoreTable (5); 
         
         assertEquals (5,x.getCapacity());
     }
     
+    //Trying to make the capacity zero
     public void testHighScoreTableIntZero() {
         HighScoreTable x = new HighScoreTable (0); 
         
         assertEquals (0,x.getCapacity());
     }
- 
+    
+    //Trying to make the capacity negative
     public void testHighScoreTableNegativeCapacity() {        
         try {
         	HighScoreTable x = new HighScoreTable (-7);
@@ -33,6 +41,7 @@ public class HighScoreTableTest extends TestCase {
         }
     }
     
+    //With the object i nthe array
     public void testSize() {
     	HighScoreTable x = new HighScoreTable (7); 
     	x.add("dfdfs", 234);
@@ -44,18 +53,30 @@ public class HighScoreTableTest extends TestCase {
     	assertEquals (5,x.size());
     }
     
+    //Empty array
     public void testSizeEmpty() {
     	HighScoreTable x = new HighScoreTable (7); 
     	
     	assertEquals (0,x.size());
     }
-
+    
+  //Size at Capacity
+    public void testSizeACapactiy() {
+    	HighScoreTable x = new HighScoreTable (2); 
+    	x.add("dfsdfsd", 12455);
+    	x.add("dffdfdsd", 13556);
+    	
+    	assertEquals (2,x.size());
+    }
+    
+    //Int parameter
     public void testGetCapacity() {
     	HighScoreTable x = new HighScoreTable (7); 
 
         assertEquals (7,x.getCapacity());
     }
     
+    //Negative parameter
     public void testGetCapacityNegative() {
     	try {
     		HighScoreTable x = new HighScoreTable (-7);
@@ -64,7 +85,8 @@ public class HighScoreTableTest extends TestCase {
     	}catch (NegativeArraySizeException e) { 
     	}
     }
-
+    
+    //Add in the right order
     public void testAdd() {
     	HighScoreTable x2 = new HighScoreTable();
     	x2.add("Christiana", 1240);
@@ -76,6 +98,7 @@ public class HighScoreTableTest extends TestCase {
         assertEquals ("Paloma", x2.getName(1));
     }
     
+    //Add out of order
     public void testAddLowerScoreFrist() {
     	HighScoreTable x2 = new HighScoreTable();
     	x2.add("Christiana", 1230);
@@ -85,6 +108,7 @@ public class HighScoreTableTest extends TestCase {
         assertEquals ("Paloma", x2.getName(0));
     }
     
+    //Add out of bound
     public void testAddIfZeroCapcity() {
     	HighScoreTable x2 = new HighScoreTable(0);
     	x2.add("Christiana", 1240);
@@ -97,6 +121,7 @@ public class HighScoreTableTest extends TestCase {
         }
     }
     
+    //Add negative score
     public void testAddNegativeScore() {
     	HighScoreTable x2 = new HighScoreTable();
     	x2.add("Christiana", -1240);
@@ -107,6 +132,7 @@ public class HighScoreTableTest extends TestCase {
     	}
     }
     
+    //Add the same score
     public void testAddSameScore() {
     	HighScoreTable x2 = new HighScoreTable();
     	x2.add("Christiana", 1240);
@@ -119,6 +145,7 @@ public class HighScoreTableTest extends TestCase {
     	assertEquals("Sabrina", x2.getName(1));	
     }
     
+    //Replace highest score with when the array is full
     public void testAddReplace() {
     	HighScoreTable x2 = new HighScoreTable(3);
     	x2.add("Christiana", 1240);
@@ -128,8 +155,11 @@ public class HighScoreTableTest extends TestCase {
     	
     	assertEquals(3340, x2.getScore(0));
     	assertEquals("Ethan", x2.getName(0));
+    	assertEquals(2240, x2.getScore(2));
+    	assertEquals("Sabrina", x2.getName(2));
     }
     
+    //Try to add a lower score on a full table 
     public void testAddToFullTable() {
     	HighScoreTable x2 = new HighScoreTable(3);
     	x2.add("Christiana", 1240);
@@ -141,7 +171,8 @@ public class HighScoreTableTest extends TestCase {
     	assertEquals(2240, x2.getScore(2));
     	assertEquals("Sabrina", x2.getName(2));
     }
-
+    
+    //Get the names
     public void testGetName() {
     	HighScoreTable x = new HighScoreTable (7); 
     	x.add("dfdfs", 234);
@@ -152,21 +183,18 @@ public class HighScoreTableTest extends TestCase {
     	
     	assertEquals ("dfdfdfdfs", x.getName(0));
     	assertEquals ("dfdeeefs", x.getName(1));
-    	assertEquals ("dfdddfs ", x.getName(3));
+    	assertEquals ("dfdddfs", x.getName(3));
     }
     
+    //get name from empty array
     public void testGetNameEmpty() {
     	HighScoreTable x = new HighScoreTable (7); 
-    	x.add("dfdfs", 234);
-    	x.add("dfdddfs", 224);
-    	x.add("dfdeeefs", 244);
-    	x.add("dfdfdfdfs", 264);
-    	x.add("dfwewedfs", 204);
     	
-    	assertEquals (null, x.getName(5));
+    	assertEquals (null, x.getName(0));
     }
-
-    public void testGetNameOutOfBounds() {
+    
+    //Get an array out of bounds
+    public void testGetNameNegative() {
     	HighScoreTable x = new HighScoreTable (7); 
     	x.add("dfdfs", 234);
     	x.add("dfdddfs", 224);
@@ -180,6 +208,7 @@ public class HighScoreTableTest extends TestCase {
     	}
     }
     
+    //Get score
     public void testGetScore() {
     	HighScoreTable x = new HighScoreTable (7); 
     	x.add("dfdfs", 234);
@@ -193,18 +222,14 @@ public class HighScoreTableTest extends TestCase {
     	assertEquals (224, x.getScore(3));
     }
     
+    //Get a score from empty index
     public void testGetScoreEmpty() {
     	HighScoreTable x = new HighScoreTable (7); 
-    	x.add("dfdfs", 234);
-    	x.add("dfdddfs", 224);
-    	x.add("dfdeeefs", 244);
-    	x.add("dfdfdfdfs", 264);
-    	x.add("dfwewedfs", 204);
-    	
-    	assertEquals (null, x.getName(6));
+    	assertEquals (null, x.getName(0));
     }
     
-    public void testGetScoreOutOfBound() {
+    //get score negative
+    public void testGetScoreNegative() {
     	HighScoreTable x = new HighScoreTable (7); 
     	x.add("dfdfs", 234);
     	x.add("dfdddfs", 224);
@@ -217,7 +242,8 @@ public class HighScoreTableTest extends TestCase {
     	}catch (ArrayIndexOutOfBoundsException e ) {
     	}
     }
-
+    
+    //Test the write to the file
     public void testWrite() throws FileNotFoundException {
         File file = new File ("High Score.txt");
         HighScoreTable x = new HighScoreTable ();
@@ -226,14 +252,15 @@ public class HighScoreTableTest extends TestCase {
     	x.add("asd", 244);
     	x.add("asdf", 264);
     	x.add("asef", 204);
-    	
+   
     	x.write(file);
     }
 
     public void testPrint() {
         fail("Not yet implemented");
     }
-
+    
+    //This check the writing to see if it is right
     public void testRead() throws FileNotFoundException {
     	File file = new File ("High Score.txt");
      	HighScoreTable x2 = HighScoreTable.read(file);
