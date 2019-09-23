dd85c5287164cf32f19510cd7851bd449c9c50fd
Christiana Zheng Wu <cz2wu@edu.uwaterloo.ca>
2019-09-23 14:08:39 -0400

    Commit

--- msci240/src/ArrayListMethods.java
+++ msci240/src/ArrayListMethods.java
@@ -1,16 +1,16 @@
 import java.util.ArrayList;
 
-public class ArrayListMethod {
-	public static void test () {
-		ArrayList <Integer> al = new ArrayList<> (5);
+public class ArrayListMethods {
+/*
+ * 	public static void main(String[] args){
+
+		ArrayList <Integer> al = new ArrayList<> (1);
 		al.add(12);
-		al.add(15);
-		al.add(3);
-		al.add(16);
-		al.add(17);
+
 		
 		minToFront(al);
-	}
+	} */
+	
 	public static void minToFront (ArrayList<Integer> al) {
 			Integer low= al.get(0);
 			int index = 0;
