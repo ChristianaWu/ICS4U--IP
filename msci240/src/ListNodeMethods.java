11326567f519f8400eaa181de2088b24845285d1
Christiana Zheng Wu <cz2wu@edu.uwaterloo.ca>
2019-10-02 13:44:08 -0400

    commit

---
+++ msci240/src/ListNodeMethods.java
@@ -0,0 +1,31 @@
+
+public class ListNodeMethods {
+
+    // before:
+    //    list -> 1 -> 2
+    // after list = append(list, 3):
+    //    list -> 1 -> 2 -> 3
+    public static ListNode append(ListNode list, int value) {
+        // TODO: add your code for Q1 here
+        return list;
+    }
+    
+    // before:
+    //    list -> 1 -> 2
+    // after list = prepend(list, 3):
+    //    list -> 3 -> 1 -> 2
+    public static ListNode prepend(ListNode list, int value) {
+        // TODO: add your code for Q2 here
+        return list;
+    }
+    
+    // before:
+    //   list -> 1 -> 2 -> 3
+    //   temp -> 4 -> 5 -> 6
+    // after:
+    //   return list -> 5 -> 3 -> 4 -> 2
+    public static ListNode rearrangeLists(ListNode list, ListNode temp) {
+        // TODO: add your code for Q3 here
+        return list;
+    }
+}
