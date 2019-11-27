47b4ee576e2352cba968d8879a3aa248772e6436
Christiana Zheng Wu <cz2wu@edu.uwaterloo.ca>
2019-11-27 11:52:41 -0500

    working on project 3

---
+++ msci240/src/QuestionTree.java
@@ -0,0 +1,12 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class QuestionTree {
+	PrintStream output;
+	Scanner input;
+	
+	public QuestionTree(Scanner input,PrintStream output) {
+		this.input = input;
+		this.output = output;
+	}
+}
